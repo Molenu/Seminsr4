e4e07f78d8213509a0100d8f9b841fd62d257ce7
Dariy_Ershova <d-r-2015@mail.ru>
2022-12-25 21:55:08 +0500

    1

---
+++ Dom1.cs
@@ -0,0 +1,11 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 3, 5 -> 243 (3⁵) 2, 4 -> 16
+Console.WriteLine("Inter the number A");
+int A =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inter the number B");
+int B =Convert.ToInt32(Console.ReadLine());
+int G=A;
+for (int SAD=1; SAD<B; SAD++)
+{
+G=G*A;
+}
+Console.WriteLine(G);
