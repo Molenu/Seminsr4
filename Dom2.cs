8556adf51a1b7651efcb6c1b2947be679a08e716
Дарья <120316778+Molenu@users.noreply.github.com>
2022-12-25 22:02:28 +0500

    nfajfh

---
+++ Dom2.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.452 -> 11 82 -> 109012 -> 12
+Console.WriteLine("Inter the number");
+int Sol=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while (Sol>0)
+{
+    int number=Sol%10;
+    Sol=Sol/10;
+    sum=sum+number;
+}
+
+Console.WriteLine(sum);
---
+++ Dom3.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]6, 1, 33 -> [6, 1, 33]
+int [] Bog()
+{
+int [] array=new int[8];
+   for (int i=0; i<array.Length;i++)
+   {
+    Console.WriteLine("enter the array elemets");
+    array[i]=Convert.ToInt32(Console.ReadLine());
+   }
+   return array ;
+}
+Console.WriteLine(string.Join(",",Bog()));
